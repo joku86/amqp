811ecc8f5f976ca688b355c5edd4b9dc70e850bf
johann.kunz <johann.kunz@TIQNB023.tiq-solutions.local>
2016-02-02 13:52:43 +0100

    rename delevop to develop

---
+++ gisa-archive-connector/src/test/java/de/tiq/solution/MainIntegrationTest.java
@@ -0,0 +1,15 @@
+package de.tiq.solution;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.tiq.solutions.archive.Main;
+
+public class MainIntegrationTest {
+	@Test
+	public void testName() throws Exception {
+		Main main = new Main();
+	}
+
+}
